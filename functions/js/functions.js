4eaff8c15c07098061f979850ba80c0d619900f7
sbusse <31662073+sbusse@users.noreply.github.com>
2018-09-23 17:31:46 -0500

    Add files via upload

--- functions/js/functions.js
+++ functions/js/functions.js
@@ -1,1 +1,19 @@
-
+function change01(){
+    document.getElementById("large").src="images/single_1_full.jpg";
+    }
+    
+    function change02(){
+    document.getElementById("large").src="images/single_2_full.jpg";
+    }
+    
+    function change03(){
+    document.getElementById("large").src="images/single_3_full.jpg";
+    }
+    
+    function change04(){
+    document.getElementById("large").src="images/single_4_full.jpg";
+    }
+    
+    function change05(){
+    document.getElementById("large").src="images/single_5_full.jpg";
+    }
