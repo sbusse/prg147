d47a96a874ffe1c2743bb871334e1adc9b0a7b51
sbusse <31662073+sbusse@users.noreply.github.com>
2018-10-03 15:27:51 -0500

    Add files via upload

--- jQuery/js/jquery.js
+++ jQuery/js/jquery.js
@@ -1,1 +1,17 @@
 
+$(document).ready(function(){
+    $("#version1").click(function(){
+        var div = $("div");
+        div.animate({left:"250px"}, "slow");
+        div.animate({height: "350px"}, "slow");
+        div.animate({width: "350px"}, "slow");
+        div.animate({fontSize: "50px"}, "slow");
+        });
+
+
+    $("#version2").click(function(){
+        $("#box2").fadeOut("slow");
+    });
+
+});
+
