5fa2dabb4e34648ce428b03e856340171fd77b7c
sbusse <31662073+sbusse@users.noreply.github.com>
2018-10-10 15:25:25 -0500

    Add files via upload

--- simple_animation_carousel/js/script.js
+++ simple_animation_carousel/js/script.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
 
+        $("#side-right").click(function(){
+            var currenSlide = $(".longLineOfImg").css("left");
+            if(parseInt(currenSlide) < -1600){
+                $(".longLineOfImg").css({
+                    left: 405
+                })
+            }
+            $(".longLineOfImg").animate({
+                left: '-=405'
+            },1000);
+        });
+
+        $("#side-left").click(function(){
+            var currenSlide = $(".longLineOfImg").css("left");
+            if(parseInt(currenSlide) >= 0){
+                $(".longLineOfImg").css({
+                    left: -2023
+                })
+            }
+            $(".longLineOfImg").animate({
+                left: '+=405'
+            },1000);
+        });
+
+});
