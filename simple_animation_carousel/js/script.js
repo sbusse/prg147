23489c46d30a43677bbfdf6b73898bf9aa2384b7
sbusse <31662073+sbusse@users.noreply.github.com>
2018-10-22 11:23:25 -0500

    Add files via upload

--- simple_animation_carousel/js/script.js
+++ simple_animation_carousel/js/script.js
@@ -1,28 +1,46 @@
 $(document).ready(function(){
-    var slider = $("slde_container");
-    num_slots = 10;
-    count = 0;
 
-        $("#right-button").click(function(){
-            count += 1;
+    "use strict";
+    var nextSlide = $("#slides img:first-child");
+    var nextCaption;
+    var nextSlideSource;
 
-            if(count >= num_slots) {
-                count = 0;
-                slider.animate({
-                    left: "-=100%"
-                })
-                slider.animate({
-                    left: "+=1000%"
-                }, 0);
-            } else {
-                slider.animate({
-                    left: "-=100%"
-                });
-            }
-        });
+    
+    $("#side-right").click(function(){
+        $("#caption").fadeOut(1500);
+        // $("#slide").fadeOut("slow");
+         $("#slide").fadeOut(1500, function(){
+            if(nextSlide.next().length === 0){  //This tests if it's at the "LAST" slide, and IF SO it starts over at the first child
+                nextSlide = $("#slides img:first-child");
+        }   else {
+                nextSlide = nextSlide.next();
+        }
+        nextSlideSource = nextSlide.attr("src"); 
+        nextCaption = nextSlide.attr("alt");
 
-        $('#left-button').click(function() {
-            alert('left');
-        });
+        $("#slide").attr("src", nextSlideSource).fadeIn(1500);
+        $("#caption").text(nextCaption).fadeIn("slow");
+
+         });
+    });
+
+    $("#side-left").click(function(){
+        $("#caption").fadeOut(1500);
+        $("#slide").fadeOut(1500, function(){
+            if(nextSlide.prev().length === 0){  //This tests if it's at the "LAST" slide, and IF SO it starts over at the first child
+                nextSlide = $("#slides img:last-child");
+        }   else {
+                nextSlide = nextSlide.prev();
+        }
+        nextSlideSource = nextSlide.attr("src"); 
+        nextCaption = nextSlide.attr("alt");
+
+        $("#slide").attr("src", nextSlideSource).fadeIn(1500);
+        $("#caption").text(nextCaption).fadeIn("slow");
+
+       });
+    });
+
+
 
 });
