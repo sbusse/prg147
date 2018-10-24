3825bd87bbeca325f1f400018d845e9c91beafca
sbusse <31662073+sbusse@users.noreply.github.com>
2018-10-24 14:11:01 -0500

    Add files via upload

--- jquery_project/js/script.js
+++ jquery_project/js/script.js
@@ -1,1 +1,42 @@
 
+ $( function() {
+     "use strict";
+  $( "#moon" ).resizable();
+} );
+
+
+
+$(document).ready(function(){
+  "use strict";
+//   $("#house").fadeIn(3000)
+  $("#zombie").fadeIn(4000);
+  $("#skull").fadeIn(5000);
+  $("#moon").fadeIn(6000);
+  
+  $("#skull").click(function(){
+      $(this).animate({
+         width: "200%",
+         height: "200%",
+         opacity: '0'}, 5000);
+  });
+
+  $("#moon").click(function(){
+    $(this).animate({
+       width: "100%",
+       height: "100%",
+       opacity: '0'}, 5000);
+    });
+
+
+//   $("#moon").click(function(){
+//     $(this).animate({
+//        width: "200%",
+//        height: "200%",
+//        opacity: '0'}, 5000);
+// });
+  
+   $("#zombie").click(function(){
+      $(this).fadeOut("slow");
+  });
+  
+});
