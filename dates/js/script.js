a600e8dcce8101f9179629b3ddfb5fc0cc5bbd53
sbusse <31662073+sbusse@users.noreply.github.com>
2018-11-12 22:11:22 -0600

    Add files via upload

--- dates/js/script.js
+++ dates/js/script.js
@@ -1,1 +1,20 @@
+var $ = function (id) {
+    return document.getElementById(id);
+}
 
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+
+function setDate(){
+    $("today").value = month + "/" + day + "/" + year;
+}
+
+function calculate(){
+    var today = new Date (("today").value);
+    var graduation = $("graduation").value;
+
+    var gmonth = (parseInt(birthday.substring(0,2)));
+    var gyear = (parseInt(graduation(6,0)));
+}
